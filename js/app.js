a0372b8746ea6aec41af902078b37a56a9ca1d1f
Benjamin Small <smallbenjamin94@gmail.com>
2022-04-08 16:06:41 -0700

    designed multiplyArray function in accordance with problem 5

--- js/app.js
+++ js/app.js
@@ -82,7 +82,7 @@ function sumArray(sumArr) { //eslint-disable-line
   for(let i = 0; i < sumArr.length; i++) {
     arraySum = sum(arraySum, sumArr[i])[0];
   }
-  let arrayString = `${sumArr} was passed in as an array of numbers, and 9 is their sum.`
+  let arrayString = `${sumArr} was passed in as an array of numbers, and 9 is their sum.`;
   return [arraySum, arrayString];
 }
 
@@ -104,11 +104,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  let arrayProduct = 1;
+  for(let i = 0; i < multArr.length; i++) {
+    arrayProduct = multiply(arrayProduct, multArr[i])[0];
+  }
+  let arrayString = `The numbers ${multArr} have a product of ${arrayProduct}.`;
+  return [arrayProduct, arrayString];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
